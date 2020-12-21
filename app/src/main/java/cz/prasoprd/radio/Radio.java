7c45dcaef0877fe759dbe6dfad9af5270e465b34
prasoprd <71968268+prasoprd@users.noreply.github.com>
2020-12-21 11:41:45 +0100

    Initial commit

---
+++ app/src/main/java/cz/prasoprd/radio/Radio.java
@@ -0,0 +1,46 @@
+package cz.prasoprd.radio;
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.view.View;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class Radio {
+
+    private static MediaPlayer player;
+
+    public static void setVolume(float volume) {
+        if (player != null) {
+            player.setVolume(volume, volume);
+        }
+    }
+
+    public static void stop(View view) {
+        if (player != null && player.isPlaying()) {
+            if (view != null) {
+                Snackbar.make(view,"Stopping player", Snackbar.LENGTH_LONG).show();
+            }
+            player.stop();
+        }
+    }
+
+    public static void play(View view, String link) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Snackbar.make(view,"Preparing for playing", Snackbar.LENGTH_LONG).show();
+                    stop(null);
+                    player = new MediaPlayer();
+                    player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                    player.setDataSource(link);
+                    player.prepare();
+                    player.start();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+}
